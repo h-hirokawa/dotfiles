1423991fd5e5872bc3a88e53c036080887f62b30
Hidetoshi Hirokawa <1086022+h-hirokawa@users.noreply.github.com>
2021-04-01 02:09:55 +0900

    feat: add dotfiles and bootstrap script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# dotfiles
+# h-hirokawa's dotfiles
+
+```bash
+$ bash -c "$(curl -L https://raw.githubusercontent.com/h-hirokawa/dotfiles/main/bootstrap.sh)"
+```
---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+SRC_DIR=~/Developer/src
+DEV_PLAYBOOK_URL=https://github.com/geerlingguy/mac-dev-playbook.git
+DEV_PLAYBOOK_PATH=${SRC_DIR}/github.com/geerlingguy/mac-dev-playbook
+DOTFILES_URL=https://github.com/h-hirokawa/dotfiles.git
+DOTFILES_PATH=${SRC_DIR}/github.com/h-hirokawa/dotfiles
+PYTHON_VERSION=$(python3 --version | awk '{sub("\\.[0-9]+$", "", $2); print $2}')
+
+xcode-select --install || true
+
+mkdir -p "$DEV_PLAYBOOK_PATH"
+git clone "$DEV_PLAYBOOK_URL" "$DEV_PLAYBOOK_PATH" 2> /dev/null \
+  || (cd "$DEV_PLAYBOOK_PATH" ; git pull)
+
+mkdir -p "$DOTFILES_PATH"
+git clone "$DOTFILES_URL" "$DOTFILES_PATH" 2> /dev/null \
+  || (cd "$DOTFILES_PATH" ; git pull)
+
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python3 /tmp/get-pip.py
+
+cd "$DEV_PLAYBOOK_PATH"
+python3 -m pip install --user ansible
+~/Library/Python/3.8/bin/ansible-galaxy install -r requirements.yml
+ln -fs "$DOTFILES_PATH/config.yml" "$DEV_PLAYBOOK_PATH/config.yml"
+~/Library/Python/${PYTHON_VERSION}/bin/ansible-playbook main.yml -i inventory --ask-become-pass
